7916462ad7903abd5c1246fdfc1a6e6b1b4dd37c
CallensJ <callens.johan@protonmail.com>
2025-05-12 09:10:54 +0200

    added unit-tests components

--- app/profile/[username]/components/SuscribeBtn.tsx
+++ app/profile/[username]/components/SuscribeBtn.tsx
@@ -3,7 +3,7 @@
 export default function SuscribeBtn() {
   return (
     <button
-      className="bg-gradient-to-r from-[#f9572a] to-[#ff9b05] text-white cursor-pointer font-semibold px-6 py-2 rounded-full shadow-md hover:brightness-110 transition-all duration-300"
+      className="bg-gradient-to-r from-[#f9572a] to-[#ff9b05] text-white cursor-pointer font-semibold px-6 py-2 shadow-md hover:brightness-110 transition-all duration-300"
     >
       S’abonner
     </button>
